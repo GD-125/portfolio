2b29cb706cf90aeaa12dca92ca93567e222ca054
Techy <110885423+GD-125@users.noreply.github.com>
2025-06-04 16:47:52 +0530

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -7,7 +7,7 @@ export function SiteFooter() {
       <div className="container flex flex-col items-center justify-between gap-4 md:flex-row px-4 md:px-6">
         <div className="flex items-center gap-2">
           <Shield className="h-6 w-6 text-primary" />
-          <span className="text-lg font-bold">Portfolio</span>
+          <span className="text-lg font-bold">GURUCHANDHRAN M</span>
         </div>
         <p className="text-center text-sm text-muted-foreground md:text-left">
           &copy; {new Date().getFullYear()} All rights reserved. Designed and Developed by{" "}
